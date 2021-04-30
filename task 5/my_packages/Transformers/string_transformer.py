bf2bb2bbdad1f57e054d2940afed4774bdb6964a
abduallh <abduallheid87@gmail.com>
2021-05-01 00:07:54 +0200

    task5

---
+++ task 5/my_packages/Transformers/string_transformer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def reverse_str(x):
+    return print(x[::-1])
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+def capitalize_str(str):
+    
+    return print(str.capitalize())
+
+
+# In[ ]:
+
+
+
+
---
+++ task 5/my_packages/operations/math.py
@@ -0,0 +1,8 @@
+def cal_average(num):
+    sum_num = 0
+    for i in num:
+        sum_num = sum_num + i           
+
+    avg = sum_num / len(num)
+    return avg
+
